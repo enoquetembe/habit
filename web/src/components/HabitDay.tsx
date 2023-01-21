49ea73efc0bdf4892c4b50fd8e5c09c08bf0e40d
enoquetembe <enoquetembetembe10@gmail.com>
2023-01-21 10:31:45 -0300

    adding checkbox

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from '@radix-ui/react-popover'
+import * as Checkbox from '@radix-ui/react-checkbox'
 import clsx from 'clsx'
 import { ProgressBar } from './ProgressBar'
+import { Check } from 'phosphor-react'
 
 interface HabitDayProps {
   completed: number,
@@ -28,6 +30,17 @@ export function HabitDay(props: HabitDayProps) {
           <span className='mt-1 font-extrabold leading-tight text-3xl'>20/01</span>
 
           <ProgressBar progress={completedPercentage}/>
+
+          <div className='mt-6 flex flex-col gap-3'>
+            <Checkbox.Root className='flex items-center gap-3 group'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
+                <Checkbox.Indicator> 
+                  <Check/>
+                </Checkbox.Indicator>
+              </div>
+              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>Beber 2L de água</span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
         </Popover.Content>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,25 +1,46 @@
+import * as Checkbox  from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
+
+const availableWeekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',  'Saturday']
 
 export function NewHabitForm() {
   return (
     <form className='w-full flex flex-col p-10 '>
-      <label htmlFor='title' className='font-semibold leading-tight'>What is your commitment</label>
-      <input 
-        type='text' 
+      <label htmlFor='title' className='font-semibold leading-tight'>
+        What is your commitment
+      </label>
+      <input
+        type='text'
         id='title'
         placeholder='egs.: Study, Code, Make exercises'
         autoFocus
         className='bg-zinc-800 text-white p-4 mt-3 rounded-lg placeholder:text-zinc-400'
       />
 
-      <label htmlFor='' className='mt-4 font-semibold font-leading'> what is the recurrence</label>
-      
-      <button 
+      <label htmlFor='' className='mt-4 font-semibold font-leading'>
+        What is the recurrence
+      </label>
+
+      <div className='mt-3 flex flex-col gap-2'>
+        {availableWeekDays.map((weekday) => (
+          <Checkbox.Root className='flex items-center gap-3 group'>
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
+              <Checkbox.Indicator>
+                <Check />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className=' text-white leading-tight'>{weekday}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
+
+      <button
         type='submit'
         className='flex items-center justify-center gap-3 mt-6 p-4 rounded-lg font-semibold bg-green-600 hover:bg-green-500'
       >
-        <Check size={20}/>
-        Confirm      
+        <Check size={20} />
+        Confirm
       </button>
     </form>
   )
