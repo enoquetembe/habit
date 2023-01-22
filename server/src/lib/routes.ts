fb7e711257be36da911ee2d45baf0ec7f1ef11d4
enoquetembe <enoquetembetembe10@gmail.com>
2023-01-22 13:16:30 -0300

    updating day route

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -65,7 +65,7 @@ export async function appRoutes(app: FastifyInstance) {
       }
     })
 
-    const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+    const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)?? []
     
     return {
       possibleHabits,
