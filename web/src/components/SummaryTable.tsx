d869b9f4c1aecd7af3c34f79b36fbbfcd9706880
enoquetembe <enoquetembetembe10@gmail.com>
2023-01-23 14:38:29 -0300

    sincronizing completd habits with api

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -44,7 +44,7 @@ export function SummaryTable() {
 
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
         {
-          summaryDates.map(date => {
+          summary.length > 0 && summaryDates.map(date => {
             const dayInSummary = summary.find(day => {
               return dayjs(date).isSame(day.date, 'day')
             })
@@ -54,7 +54,7 @@ export function SummaryTable() {
                 key={date.toString()}
                 date={date}
                 amount={dayInSummary?.amount}
-                completed={dayInSummary?.completed}
+                defaultCompleted={dayInSummary?.completed}
               />
             )
           })
