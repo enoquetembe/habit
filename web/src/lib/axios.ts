2b0edd1a4e6cb07d6968bde6a9e1bde217e2d75c
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-08 19:39:01 -0300

    changing baseurl

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3000'
+  baseURL: "https://habitapi.onrender.com",
 })
 
 api.interceptors.request.use((request: AxiosRequestConfig) => {
