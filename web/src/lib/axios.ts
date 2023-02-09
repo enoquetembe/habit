dcb8cd6d0869ef1f1fc930740aa100827bea8864
enoquetembe <enoquetembetembe10@gmail.com>
2023-02-09 09:13:55 -0300

    updating base url

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://habitapi.onrender.com",
+  baseURL: "https://apihabit.onrender.com",
 })
 
 api.interceptors.request.use((request: AxiosRequestConfig) => {
